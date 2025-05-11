2071065065e21819837af0152ebbbed9fdcbf8a8
mwafrika <mwafrikajosue@gmail.com>
2025-05-11 15:30:44 +0200

    setup initial configurations for the backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-console": "error",
+      "no-undef": "error",
+      "no-unused-vars": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+      eqeqeq: ["error", "always"],
+      "no-duplicate-imports": "error",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+]);
