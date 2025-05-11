31f11e2bff363a8760d5bdde8d25caaf5d18e795
mwafrika <mwafrikajosue@gmail.com>
2025-05-11 16:31:08 +0200

    update App.js for mobile

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NetworkProvider } from "./contexts/NetworkContext";
+import { useSync } from "./hooks/useSync";
+import NotesListScreen from "./screens/NotesListScreen";
+import CreateNoteScreen from "./screens/CreateNoteScreen";
+import { registerForPushNotificationsAsync } from "./notifications/notifications";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  useSync();
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NetworkProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Notes" component={NotesListScreen} />
+          <Stack.Screen name="Create" component={CreateNoteScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NetworkProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
